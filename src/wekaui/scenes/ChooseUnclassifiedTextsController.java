622a897f9de37086e0ab8788f0154df9d1af46d6
ElwoodSlut <markus.uderhardt@stud.uni-regensburg.de>
2015-02-05 14:06:05 +0100

    updated chooseunclassifiedtexts listview

--- src/wekaui/scenes/ChooseUnclassifiedTextsController.java
+++ src/wekaui/scenes/ChooseUnclassifiedTextsController.java
@@ -8,12 +8,15 @@ package wekaui.scenes;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -22,7 +25,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
@@ -31,6 +36,7 @@ import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import javafx.util.Callback;
 
 import wekaui.Session;
 
@@ -54,25 +60,74 @@ public class ChooseUnclassifiedTextsController implements Initializable {
     private Label dropzoneLabel;
     @FXML
     private ListView<File> dropzoneListView;
+    @FXML
+    private Button clearButton;
+    @FXML
+    private Button addButton;
     
     private static final ObservableList<File> dataList = 
             FXCollections.observableArrayList();
     
-    private Session session;    
+    private Session session; 
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        initModelDropzone();
+        initDataDropzone();
         
         session = new Session();
         
-        dropzoneListView.setItems(dataList);
+        initListView();
     }    
     
-    private void initModelDropzone() {        
+    private void initListView(){
+        dropzoneListView.setItems(dataList);
+        
+        dropzoneListView.setCellFactory((ListView<File> list) -> {
+            
+            final ListCell cell = new ListCell<File>(){
+                @Override
+                public void updateItem(File item, boolean empty) {
+                    super.updateItem(item, empty);
+                    setText(empty ? null : getString());                        
+                }
+
+                private String getString() {
+                    return getItem() == null ? "" : getItem().toString();
+                }
+            };
+            
+            Tooltip deleteTip = new Tooltip("Click to delete");
+            
+            cell.setOnMouseEntered((MouseEvent event) -> {
+                if(cell.getItem() != null){
+                    double x = event.getScreenX();
+                    double y = event.getScreenY();                    
+                    deleteTip.show(cell, x + 10, y + 5);
+                }
+            });
+            
+            cell.setOnMouseExited((MouseEvent event) -> {
+                if(cell.getItem() != null){
+                    deleteTip.hide();
+                }
+            });
+            
+            cell.setOnMouseClicked((MouseEvent event) -> {
+                if(cell.getItem() != null){                    
+                    dataList.remove((File)cell.getItem());
+                    deleteTip.hide();
+                    checkIfDatalistIsEmpty();
+                }
+            });
+            return cell;
+        });
+        
+    }
+    
+    private void initDataDropzone() {        
         dropzoneArea.setOnDragEntered((DragEvent event) -> {                        
             dropzoneLabel.getStyleClass().add("active");
         });
@@ -104,32 +159,18 @@ public class ChooseUnclassifiedTextsController implements Initializable {
                     dataList.add(file);                                        
                 }
                 session.setUnlabeledData(dataList);
+                changeDataButtonsVisibility(true);
                 success = true;
             }
             event.setDropCompleted(success);
             event.consume();
-            
-            System.out.println(dataList);
         });
         
         /**
         * Show file chooser if user clicks on the dropzone instead of dragging a file into it
         */
         dropzoneArea.setOnMouseClicked((MouseEvent event) -> {
-            System.out.println("area clicked");
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Unklassifizierte Daten öffnen");
-            Window window = ((Node)event.getTarget()).getScene().getWindow();
-            
-            File dataFile = fileChooser.showOpenDialog(window);
-            
-            if(dataFile != null) {                
-                if(dropzoneListView.getItems().size() == 0){
-                    dropzoneLabel.setVisible(false);
-                }
-                dataList.add(dataFile);                
-                session.setUnlabeledData(dataList);
-            }
+            startChooseFileDialog(event);            
         });
     }
 
@@ -160,7 +201,56 @@ public class ChooseUnclassifiedTextsController implements Initializable {
         }
                 */
     }
-
-
     
+    /**
+     * Clears the dataList and the UnlabeledData from the Session
+     * @param event 
+     */
+    @FXML
+    private void onClearClicked(ActionEvent event) {
+        dataList.clear();        
+        session.setUnlabeledData(dataList);
+        dropzoneLabel.setVisible(true);
+        changeDataButtonsVisibility(false);        
+    }
+
+    @FXML
+    private void onAddClicked(ActionEvent event) {
+        startChooseFileDialog(event);
+    }
+    
+    private void changeDataButtonsVisibility(boolean visibility){
+        addButton.setVisible(visibility);
+        clearButton.setVisible(visibility);
+    }
+
+    private void startChooseFileDialog(Event event) {
+        FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Unklassifizierte Daten öffnen");
+            
+            Window window = ((Node)event.getTarget()).getScene().getWindow();
+            
+            List<File> dataFile = fileChooser.showOpenMultipleDialog(window);
+            
+            if(dataFile != null) {                
+                if(dropzoneListView.getItems().size() == 0){
+                    dropzoneLabel.setVisible(false);
+                }
+                
+                for(File file:dataFile){
+                    dataList.add(file);
+                }
+                
+                session.setUnlabeledData(dataList);
+                changeDataButtonsVisibility(true);
+            }
+    }
+
+    private void checkIfDatalistIsEmpty() {
+        if(dataList.isEmpty()){
+            dropzoneLabel.setVisible(true);
+        }
+            
+    }
+   
 }
