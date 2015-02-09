db8ad195331a59f6394107e556f46f3ee01e0106
ElwoodSlut <markus.uderhardt@stud.uni-regensburg.de>
2015-02-09 14:20:27 +0100

    added first draft of infodialog

---
+++ src/wekaui/customcontrols/InfoDialogController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wekaui.customcontrols;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author FreshXL
+ */
+public class InfoDialogController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
