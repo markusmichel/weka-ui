339d9e4f91abeab870442217ed212127902fef4d
Noodlewood <daniel.riedmuller@gmail.com>
2015-02-10 18:26:01 +0100

    update

--- src/wekaui/logic/arffconverter/ArffConverter.java
+++ src/wekaui/logic/arffconverter/ArffConverter.java
@@ -8,27 +8,44 @@ package wekaui.logic.arffconverter;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 /**
  *
  * @author Noodlewood
  */
 public class ArffConverter {
+
     private DataToArff dataToArff;
 
-    public File convert(File data) {
-        try {
-            String type = Files.probeContentType(data.toPath());
-            if (type == null) {
-                System.err.format("'%s' has an" + " unknown filetype.%n", data);
-            } else {
-                System.out.println("file is type " + type);
-                dataToArff = new TxtToArff();
-            }
-        } catch (IOException x) {
-            System.err.println(x);
-        }
+    public File makeArffFile(List<File> data) {
+        List<File> arffFiles  = null;
         
-        return dataToArff.convertToArff(data);
+        for (File orgFile : data) {
+            try {
+                String type = Files.probeContentType(orgFile.toPath());
+                if (type == null) {
+                    System.err.format("'%s' has an" + " unknown filetype.%n", data);
+                } else {
+                    System.out.println("file is type " + type);
+                    dataToArff = new TxtToArff();
+                    arffFiles.add(dataToArff.convertToArff(orgFile));
+                }
+            } catch (IOException x) {
+                System.err.println(x);
+            }
+        }
+
+        return mergeArffFiles(arffFiles);
+    }
+    
+    private File mergeArffFiles(List<File> arffFiles) {
+        File mergedArff = null;
+        /*
+        for (File arffFile : arffFiles) {
+            
+        }
+        */
+        return mergedArff;
     }
 }
