7928c7747a871e6ef8aa856e790dce5eae93584f
Noodlewood <daniel.riedmuller@gmail.com>
2015-02-18 14:08:20 +0100

    ArffFileIncompatibleException implemented

--- src/wekaui/logic/Trainer.java
+++ src/wekaui/logic/Trainer.java
@@ -5,6 +5,8 @@
  */
 package wekaui.logic;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import weka.core.Instances;
 import wekaui.LastUsedModel;
 
@@ -17,17 +19,27 @@ public class Trainer {
     String modelPath = "train.model";
     String testdataPath = "test.arff";
     
-    public static MyInstances classifyData(LastUsedModel model, MyInstances unlabeled) throws Exception {
+    public static void classifyData(LastUsedModel model, MyInstances unlabeled) throws ArffFileIncompatibleException {
         unlabeled.setClassIndex(unlabeled.numAttributes() - 1);
         Instances labeled = new Instances(unlabeled);
 
         for (int i = 0; i < unlabeled.numInstances(); i++) {
-          double clsLabel = model.getModel().classifyInstance(unlabeled.instance(i));
-          labeled.instance(i).setClassValue(clsLabel);
+          double clsLabel;
+            try {
+                clsLabel = model.getModel().classifyInstance(unlabeled.instance(i));
+                labeled.instance(i).setClassValue(clsLabel);
           
-          unlabeled.getMyInstances().add(new MyInstance(labeled.instance(i), model.getModel().distributionForInstance(labeled.instance(i))));
+                unlabeled.getMyInstances().add(new MyInstance(labeled.instance(i), model.getModel().distributionForInstance(labeled.instance(i))));
+            } catch (Exception ex) {
+                throw new ArffFileIncompatibleException();
+            }          
         }       
-        
-        return unlabeled;
     }    
+
+    public static class ArffFileIncompatibleException extends Exception {
+
+        public ArffFileIncompatibleException() {
+            System.out.println("Arff File incompatible!");
+        }
+    }
 }
