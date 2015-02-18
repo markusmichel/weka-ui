df1b4c19d48c100fdac4d479d80a4ab0836d50a1
ElwoodSlut <markus.uderhardt@stud.uni-regensburg.de>
2015-02-18 14:21:12 +0100

    result layout update

--- src/wekaui/scenes/result/ResultMainController.java
+++ src/wekaui/scenes/result/ResultMainController.java
@@ -7,7 +7,15 @@ package wekaui.scenes.result;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import wekaui.Session;
 
 /**
@@ -19,14 +27,37 @@ public class ResultMainController implements Initializable {
 
     private Session session;
     
+    @FXML
+    private Button exportBtn;
+    @FXML
+    private BorderPane container;
+    @FXML
+    private GridPane gridPane;
+    
     /**
      * Initializes the controller class.
      */    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        initializePieChart();
     }    
     
+    private void initializePieChart(){
+        
+        ObservableList<PieChart.Data> pieChartData =
+                FXCollections.observableArrayList(
+                new PieChart.Data("Grapefruit", 13),
+                new PieChart.Data("Oranges", 25),
+                new PieChart.Data("Plums", 10),
+                new PieChart.Data("Pears", 22),
+                new PieChart.Data("Apples", 30));
+        final PieChart chart = new PieChart(pieChartData);
+        chart.setTitle("Imported Fruits");        
+        
+        gridPane.add(chart,1,1,1,2);
+    }
     
     public void setSession(Session s){
         this.session = s;
