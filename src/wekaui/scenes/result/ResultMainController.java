1303a85e4ba679f88b0032c516e7b104e955f56c
ElwoodSlut <markus.uderhardt@stud.uni-regensburg.de>
2015-02-19 08:46:07 +0100

    added first result implementation

--- src/wekaui/scenes/result/ResultMainController.java
+++ src/wekaui/scenes/result/ResultMainController.java
@@ -6,17 +6,25 @@
 package wekaui.scenes.result;
 
 import java.net.URL;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
 import wekaui.Session;
+import wekaui.logic.MyInstance;
+import wekaui.logic.MyInstances;
 
 /**
  * FXML Controller class
@@ -34,33 +42,94 @@ public class ResultMainController implements Initializable {
     @FXML
     private GridPane gridPane;
     
+    private LinkedHashMap<String, List<MyInstance>> list;
+    
     /**
      * Initializes the controller class.
      */    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
         
-        initializePieChart();
     }    
     
-    private void initializePieChart(){
+    /**
+     * Initializes the pie chart
+     */
+    private void initializePieChart(List<MyInstances> classifiedData){
         
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                new PieChart.Data("Grapefruit", 13),
-                new PieChart.Data("Oranges", 25),
-                new PieChart.Data("Plums", 10),
-                new PieChart.Data("Pears", 22),
-                new PieChart.Data("Apples", 30));
+        ObservableList<PieChart.Data> pieChartData = preparePieChartData(classifiedData);        
+        
         final PieChart chart = new PieChart(pieChartData);
-        chart.setTitle("Imported Fruits");        
         
+        for (final PieChart.Data data : chart.getData()) {
+            
+            Tooltip pieTip = new Tooltip("Amount: " + data.getPieValue());
+            
+            // adds the mouse enter event: ToolTip is shown
+            data.getNode().addEventHandler(MouseEvent.MOUSE_ENTERED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        double x = e.getScreenX();
+                        double y = e.getScreenY();                    
+                        pieTip.show(data.getNode(), x + 10, y + 5);
+                     }
+                });
+            
+            // adds the mouse leave event: ToolTip is removed
+            data.getNode().addEventHandler(MouseEvent.MOUSE_EXITED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        pieTip.hide();
+                     }
+                });
+            
+            // adds the mouse click event: the data of the pie slice is shown
+            data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED,
+                new EventHandler<MouseEvent>() {
+                    @Override public void handle(MouseEvent e) {
+                        System.out.println("PieData: " + data);
+                        List<MyInstance> l = getPieData(data.getName());
+                        System.out.println("LIST: " + l);
+                     }
+                //returns the associated data of the pie slice
+                private List<MyInstance> getPieData(String name) {
+                    return list.get(name);
+                }
+            });
+        }
+                
         gridPane.add(chart,1,1,1,2);
     }
     
+    /**
+     * Sets the session
+     * @param s Session object which contains the data
+     */
     public void setSession(Session s){
-        this.session = s;
+        this.session = s;        
+        
+        initializePieChart(this.session.getUnlabeledData());
+    }
+    
+    /**
+     * Prepares the data for the PieChart.
+     * @param classifiedData List which contains the MyInstances
+     * @return PieChartData
+     */
+    private ObservableList<PieChart.Data> preparePieChartData(List<MyInstances> classifiedData) {
+        
+        // merges the data
+        list = MyInstances.getMergedData(classifiedData);
+        
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();        
+        Iterator it = list.entrySet().iterator();
+        
+        while(it.hasNext()){
+            Map.Entry pair = (Map.Entry) it.next();
+            int value = ((List)pair.getValue()).size();
+            pieChartData.add(new PieChart.Data((String)pair.getKey(), value));
+        }
+        return pieChartData;        
     }
     
 }
